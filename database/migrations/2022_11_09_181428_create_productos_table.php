ea32aa9368c2901863c6eb17f361c1fcc5057c37
Santos2102 <mikesan2102@umes.edu.gt>
2022-11-09 12:40:43 -0600

    Creación de la base de datos y Login

---
+++ database/migrations/2022_11_09_181428_create_productos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string("name",255);
+            $table->string("description",255);
+            $table->string("brand",255);
+            $table->float("price");
+            $table->integer("stock");
+            $table->string("image");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+};
